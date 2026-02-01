7b99ec9078325f08284aad35a96cb6f2286038c1
abaikhan <abil.abaikhan@gmail.com>
2026-02-01 19:17:11 +0500

    Migration to Spring

--- src/main/java/aviation/InvalidFlightDataException.java
+++
@@ -1,6 +0,0 @@
-package aviation;
-public class InvalidFlightDataException extends Exception {
-    public InvalidFlightDataException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/aviation/flight_app/FlightAviationAppApplication.java
@@ -0,0 +1,13 @@
+package com.aviation.flight_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAviationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAviationAppApplication.class, args);
+	}
+
+}
