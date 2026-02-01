c71deeb0b8115f21d7abdfd70e2238f92ab8d7fc
abaikhan <abil.abaikhan@gmail.com>
2026-02-01 23:56:19 +0500

    aa

--- src/main/java/com/aviation/flight_app/InvalidFlightDataException.java
+++
@@ -1,7 +0,0 @@
-package com.aviation.flight_app;
-
-public class InvalidFlightDataException extends Exception {
-    public InvalidFlightDataException(String message) {
-        super(message);
-    }
-}
